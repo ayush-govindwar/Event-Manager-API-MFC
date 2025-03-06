4f73b44997bd81012ea7998b1388631d51f2e7e1
ayush-govindwar <swayush.govindwar@gmail.com>
2025-03-06 20:53:38 +0530

    added security measures

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 //rest of the packages
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
 
 //database
 const connectDB = require('./db/connect');
@@ -19,14 +22,21 @@ const eventRouter = require('./routes/eventRoutes');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-//testing
-
-
+testing
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser(process.env.JWT_SECRET));
+app.use(helmet());
+app.use(xss());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/event', eventRouter);
